c748f07cb04bb087f911ff3b580ad76174725fec
Justudent <justudent09@mail.ru>
2025-04-13 21:20:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -165,13 +165,29 @@ function updateTaskList(date) {
             const targetDateValue = jsDateToExcelDate(new Date(dateKey));
             let targetRowIndex = -1;
 
-            for (let i = 0; i < jsonData.length; i++) {
-                const cellValue = jsonData[i][1]; 
-                if (typeof cellValue === 'number' && cellValue === targetDateValue) {
-                    targetRowIndex = i;
-                    break;
-                }
-            }
+for (let i = 0; i < jsonData.length; i++) {
+    const cellValue = jsonData[i][1];
+
+    // Попытка сопоставить число (excel-дата)
+    if (typeof cellValue === 'number' && cellValue === targetDateValue) {
+        targetRowIndex = i;
+        break;
+    }
+
+    // Попытка сопоставить строку формата 'ДД.ММ.ГГГГ'
+    if (typeof cellValue === 'string') {
+        const [day, month, year] = cellValue.split('.');
+        const cellDate = new Date(+year, +month - 1, +day);
+        if (
+            cellDate.getFullYear() === date.getFullYear() &&
+            cellDate.getMonth() === date.getMonth() &&
+            cellDate.getDate() === date.getDate()
+        ) {
+            targetRowIndex = i;
+            break;
+        }
+    }
+}
 
             if (targetRowIndex !== -1) {
                 const timeSlots = [
